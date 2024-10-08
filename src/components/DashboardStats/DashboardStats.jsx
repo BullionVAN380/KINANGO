f871d8bf39b55fe3e5fa799e3f1c570ed9d0d5d0
BullionVAN380 <josphatbaraka3@gmail.com.com>
2024-10-08 09:41:30 +0300

    round up add

--- src/components/DashboardStats/DashboardStats.jsx
+++ src/components/DashboardStats/DashboardStats.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaUserGraduate, FaChalkboardTeacher, FaBook, FaAward } from 'react-icons/fa';
+import CountUp from 'react-countup'; // Importing CountUp for number animations
 
 const StatsCard = ({ icon, number, label }) => {
   return (
-    <div className="col-md-3 text-center">
+    <div className="col-md-3 col-sm-6 text-center mb-4">
       <div className="mb-3">
         {icon}
       </div>
-      <h1>{number}</h1>
+      {/* Animated CountUp for numbers */}
+      <h1>
+        <CountUp end={number} duration={3} /> {/* Animates counting to the target number */}
+      </h1>
       <p>{label}</p>
     </div>
   );
@@ -20,22 +24,22 @@ const DashboardStats = () => {
       <div className="row">
         <StatsCard 
           icon={<FaUserGraduate className='icon-color fa-2x' size={50} />} 
-          number="2,000" 
+          number={2000} 
           label="Total Number of Trainees" 
         />
         <StatsCard 
           icon={<FaChalkboardTeacher className='icon-color fa-2x' size={50} />} 
-          number="34" 
+          number={40} 
           label="Number of Trainers" 
         />
         <StatsCard         
           icon={<FaBook className='icon-color fa-2x' size={50} />} 
-          number="15" 
+          number={15} 
           label="Number of Programs" 
         />
         <StatsCard          
           icon={<FaAward className='icon-color fa-2x' size={50} />} 
-          number="2" 
+          number={2} 
           label="Number of Examination Boards" 
         />
       </div>
